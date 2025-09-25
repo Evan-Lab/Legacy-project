5e9a0f2f551d21a903969c3a0a3517a6e5fbdae7
Edouard Belhomme <edouard.belhomme@epitech.eu>
2025-09-25 13:37:10 +0200

    FEAT(cypress): update the cypress config file

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,12 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      baseUrl: "http://localhost:8000",
-      specPattern: "tests/cypress/e2e/**/*.cy.{js}",
-      supportFile: "tests/cypress/support/e2e.js",
-      video: false,
-      viewportWidth: 1366,
-      viewportHeight: 850
+      
     },
   },
 });
