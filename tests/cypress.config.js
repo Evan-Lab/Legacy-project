a4e0e9e1e90a4718c528b819fe447454515a6375
Baptiste <baptiste.perard@epitech.eu>
2025-10-05 19:53:59 +0200

    feat(Config Cypress): Adding the config to connect to website

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      
+      config.baseUrl = "http://localhost:2317";
+      return config;
     },
   },
 });
