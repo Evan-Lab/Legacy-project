d5d679baabd28cb66abb5bf69944c012395192d6
Baptiste <baptiste.perard@epitech.eu>
2025-10-05 19:55:51 +0200

    remove(Older tests): remove older test not functional

--- tests/cypress/e2e/04_family_page.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Family page", () => {
-  beforeEach(() => cy.login("visitor"));
-
-  it("shows union and children", () => {
-    cy.openFamily("F1");
-    cy.get('[data-testid="family-union"]').should("be.visible");
-    cy.get('[data-testid="children-list"] li').its("length").should("be.gte", 0);
-  });
-});
--- tests/cypress/e2e/12_history_and_diff.cy.js
+++
@@ -1,9 +0,0 @@
-describe("History & diff", () => {
-  beforeEach(() => cy.login("wizard"));
-
-  it("shows history entries and opens diff", () => {
-    cy.visit("/history");
-    cy.get('[data-testid="history-row"]').first().click();
-    cy.get('[data-testid="diff-view"]').should("be.visible");
-  });
-});
