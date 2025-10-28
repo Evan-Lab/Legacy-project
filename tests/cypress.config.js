7e10c6de80479bdacb94ce7b3b75f60a81a1e8c6
Baptiste <baptiste.perard@epitech.eu>
2025-10-28 20:47:00 +0100

    fix(Coverage and failure): Remove coverage and associate config, fix return error when tests failed

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,16 +1,15 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:2317',
     video: true,
     screenshotOnRunFailure: true,
     videosFolder: 'cypress/videos',
     screenshotsFolder: 'cypress/screenshots',
     setupNodeEvents(on, config) {
-      // Configuration pour le code coverage
-      require('@cypress/code-coverage/task')(on, config);
+      config.baseUrl = "http://localhost:2317";
       return config;
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
