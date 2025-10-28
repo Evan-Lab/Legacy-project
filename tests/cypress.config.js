8f6b9d5e315b65c2dd56247ea2bf7815d05964aa
Baptiste <baptiste.perard@epitech.eu>
2025-10-28 21:17:06 +0100

    fix(Configuration Cypress): Adding configuration to save videos and screenshot only on error

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,10 +7,32 @@ module.exports = defineConfig({
     screenshotOnRunFailure: true,
     videosFolder: 'cypress/videos',
     screenshotsFolder: 'cypress/screenshots',
+
     setupNodeEvents(on, config) {
+      // Définition du baseUrl
       config.baseUrl = "http://localhost:2317";
+
+      // Supprime les vidéos des specs sans échec
+      on("after:spec", (spec, results) => {
+        if (results && results.video) {
+          const hasFailedTests = results.tests.some(test =>
+            test.attempts.some(attempt => attempt.state === "failed")
+          );
+
+          if (!hasFailedTests) {
+            console.log(`🧹 Suppression de la vidéo (aucune erreur): ${results.video}`);
+            fs.unlink(results.video, err => {
+              if (err) {
+                console.error(`Erreur lors de la suppression de ${results.video}:`, err);
+              }
+            });
+          }
+        }
+      });
+
       return config;
     },
+
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
